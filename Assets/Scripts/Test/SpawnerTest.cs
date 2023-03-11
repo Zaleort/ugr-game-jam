9abb5af7a9eeeaa1e4d393391ee56ae36172b574
Joansito <joanmanel>
2023-03-11 18:01:56 +0100

    All

---
+++ Assets/Scripts/Test/SpawnerTest.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerTest : MonoBehaviour
+{
+    public GameObject enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Instantiate(enemy, this.transform.position, this.transform.rotation);
+        }
+    }
+
+}
