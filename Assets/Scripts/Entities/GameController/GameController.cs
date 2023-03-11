96130ff1bcfc2cb803f0853516ad85cc59ff3a4b
Alexandre Henrique Alves <zaleortgames@outlook.com>
2023-03-11 12:34:16 +0100

    GameController base

---
+++ Assets/Scripts/Entities/GameController/GameController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public void LevelUp(int level)
+    {
+        // Select upgrade
+    }
+
+    public void Death()
+    {
+        // GameOver
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Entities/GameController/GameControllerEvents.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerEvents : MonoBehaviour
+{
+    public static string LevelUp = "LevelUp";
+    public static string Death = "Death";
+}
